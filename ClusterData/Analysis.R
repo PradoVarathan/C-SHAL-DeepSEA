d659dd4b01cfb91d37b45ff08ac867a376b04536
Pradeep Varathan <pradeepvarathan.98@gmail.com>
2020-12-16 08:19:57 -0500

    Update on Code and cluster

--- ClusterData/Analysis.R
+++ ClusterData/Analysis.R
@@ -19,9 +19,30 @@ gwas_2000 = read.csv("Top_2000_IGAP_SNPs.csv",header = T)
 
 # BiocManager::install("QUBIC")
 library("QUBIC")
-results = read.csv("Results_Actual.csv",header = T)
+results = read.csv("~/Documents/GitHub/C-SHAL-DeepSEA/Data/Results_Actual.csv",header = T)
 row.names(results) = results$X
 results$X = NULL
+
+res_temp = abs(results)
+res_temp_logi = res_temp > max(res_temp)*0.35
+res_temp = results*res_temp_logi
+for (i in 1:nrow(res_temp)){
+  if (sum(res_temp[i,]) == 0){
+    res_temp[i,] = NA
+  }
+}
+res_temp_r = na.omit(res_temp)
+for (i in 1:ncol(res_temp_r)){
+  if (sum(res_temp_r[,i]) == 0){
+    res_temp_r[,i] = NULL
+  }
+}
+res_temp_r = na.omit(res_temp_r)
+
+library(pheatmap)
+dim(res_temp_r)
+pheatmap(res_temp_r)
+heatmap(as.matrix(res_temp_r))
 # BiCluster - QUBIC -------------------------------------------------------
 results_mt = as.matrix(results)
 RSID_Names = rownames(results_mt)
@@ -31,11 +52,11 @@ results_mt[1:5,1:5]
 res <- biclust::biclust(results_mt, method = BCQU(),verbose = TRUE)
 summary(res)
 
-quheatmap(results_mt, res, number = c(3,6), showlabel = TRUE)
+quheatmap(results_mt, res, number = 1, showlabel = TRUE)
 
 library(RColorBrewer)
 paleta <- colorRampPalette(rev(brewer.pal(11, "RdYlBu")))(11)
-quheatmap(results_mt, res, number = c(3,6), showlabel = TRUE, col = paleta)
+quheatmap(results_mt, res, number = c(90,84), showlabel = TRUE, col = paleta)
 
 hmcols <- colorRampPalette(rev(c("#D73027", "#FC8D59", "#FEE090", "#FFFFBF",
                                  "#E0F3F8", "#91BFDB", "#4575B4")))(100)
@@ -89,11 +110,19 @@ library(ggplot2)
 ggplot(sig_res) +
   geom_bar( aes(x=ClusterNum, y=Avg_P), stat="identity", fill="skyblue", alpha=0.7) 
 
-snps_3 = get_snp_list(res,CHMATIN_Names,3,RSID_Names)
-crms_3 = get_crms_list(res,CHMATIN_Names,3,RSID_Names)
-snps_6 = get_snp_list(res,CHMATIN_Names,6,RSID_Names)
-crms_6 = get_crms_list(res,CHMATIN_Names,6,RSID_Names)
+snps_3 = get_snp_list(res,CHMATIN_Names,34,RSID_Names)
+crms_3 = get_crms_list(res,CHMATIN_Names,34,RSID_Names)
+snps_6 = get_snp_list(res,CHMATIN_Names,84,RSID_Names)
+crms_6 = get_crms_list(res,CHMATIN_Names,84,RSID_Names)
 
+sig_res = sig_res %>% arrange(Avg_P)
+jpeg("All_Clusters.jpeg")
+par(mfrow=c(2,4))
+for (i in 1:8){
+  quheatmap(results_mt, res,number = sig_res$ClusterNum[i], col = hmcols, showlabel = TRUE)
+  
+}
+dev.off()
 # XGR Analysis  -----------------------------------------------------------
 
 # BiocManager::install("remotes", dependencies=T)
@@ -102,17 +131,19 @@ library(XGR)
 library(RCircos)
 
 # Cluster3
-XGR_3 = xEnricherSNPs(snps_3)
+XGR_3 = xEnricherSNPs(snps_3,include.LD = 'EUR')
 genes_3 = xSNP2nGenes(snps_3)
 view_3 = xEnrichViewer(XGR_3)
-ld_scores_3 = xSNPscores(snps_3)
+write.csv(view_3,"XGR_Clus34.csv",quote=F,row.names = F)
+data = data.frame("SNP"=gwas_2000$SNP[which(gwas_2000$SNP %in% snps_3)],"P" = gwas_2000$P[which(gwas_2000$SNP %in% snps_3)])
+ld_scores_3 = xSNPscores(data)
+write.csv(ld_scores_3,"LD_Scores_Clus34.csv",quote = F,row.names = F)
 social_snps_3 = xSocialiserSNPs(snps_3)
 xCircos(social_snps_3)
 xSocialiserDAGplot(social_snps_3)
 xEnrichChord(XGR_3)
-clus3_p = get_snp_p_vals(snps_3,gwas_2000)
-snps_genes_con_ld_3 = xSubneterSNPs(clus3_p)
-xSocialiserDAGplot(snps_genes_con_ld_3)
+snps_genes_con_ld_3 = xSubneterSNPs(data)
+plot(snps_genes_con_ld_3)
 
 # Cluster6
 XGR_6 = xEnricherSNPs(snps_6)
@@ -126,4 +157,4 @@ xEnrichChord(XGR_6)
 xSubneterSNPs(snps_6)
 clus6_p = get_snp_p_vals(snps_6,gwas_2000)
 snps_genes_con_ld_6 = xSubneterSNPs(clus6_p)
-xSocialiserDAGplot(snps_genes_con_ld_6)
+plot(snps_genes_con_ld_6)
